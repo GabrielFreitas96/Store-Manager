d6c06ab32698d476a2a8d8f38f8dad91888efb2b
GabrielFreitas96 <gabrielbfreitas96@gmail.com>
2022-05-30 20:22:31 -0300

    refactor: adding index.js app.js

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 const app = require('./app');
 require('dotenv').config();
-const routerProduct = require('./routers/routerProduct');
-const routerSale = require('./routers/routerSale');
-// não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
-app.get('/lalalala', (req, res) => {
-  console.log('rodando lalalala');
-  res.status(200).json({ message: 'está rodando' });
-});
 
-app.use('/products', routerProduct);
-app.use('/sales', routerSale);
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
 });
