abe2135384d6c2f4728e229ae5d9d2bf3d6ce479
GabrielFreitas96 <gabrielbfreitas96@gmail.com>
2022-05-31 16:25:19 -0300

    refactor: Requirement 7, changing the middleware verifySale to receive an array

--- middlewares/verifySale.js
+++ middlewares/verifySale.js
@@ -1,17 +1,24 @@
 const verifySale = (req, res, next) => {
-  const [{ productId }, { quantity }] = req.body;
-  console.log('No middleware de Sale productId', productId);
-  console.log('No middleware de Sale quantity', quantity);
+  console.log('body', req.body);
+  const array = req.body;
+  console.log('tamanho do array', array.length);
+  // const [{ productId, quantity }] = req.body;
+  // console.log('No middleware de Sale productId', productId);
+  // console.log('No middleware de Sale quantity', quantity);
 
-  if (!productId) {
-    return res.status(400).json({ message: '"productId" is required' });
-  }
-  if (!quantity) {
-    return res.status(400).json({ message: '"quantity" is required' });
-  }
-  if (quantity <= 0) {
-    return res.status(422).json({ message: ' "quantity" must be greater than or equal to 1' });
-  }
+  array.forEach(({ productId, quantity }) => {
+    console.log(productId, quantity);
+    if (!productId) {
+      return res.status(400).json({ message: '"productId" is required' });
+    }
+    if (!quantity) {
+      return res.status(400).json({ message: '"quantity" is required' });
+    }
+    if (quantity <= 0) {
+      return res.status(422).json({ message: ' "quantity" must be greater than or equal to 1' });
+    } 
+  });
+
   next();
 };
 
