a3c25be9b2d6ae85f101be917e234fb2e3b057d7
GabrielFreitas96 <gabrielbfreitas96@gmail.com>
2022-06-01 11:40:15 -0300

    feat: Requirement 8, adding routerSale for PUT sale/:id

--- routers/routerSale.js
+++ routers/routerSale.js
@@ -1,9 +1,11 @@
 const routerSales = require('express').Router();
 const saleController = require('../controllers/saleController');
 const { verifySale } = require('../middlewares/verifySale');
+const verifyEditSale = require('../middlewares/verifyEditSale');
 
 routerSales.get('/:id', saleController.getSalesById);
 routerSales.get('/', saleController.getSales);
 routerSales.post('/', verifySale, saleController.addSale);
+routerSales.put('/:id', verifyEditSale, saleController.editSale);
 
 module.exports = routerSales;
