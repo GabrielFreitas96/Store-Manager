3acc272380d2d12ce945cb0dbecc1f27dab2015c
GabrielFreitas96 <gabrielbfreitas96@gmail.com>
2022-06-03 11:34:17 -0300

    feat: Requirement 13, unit test for saleService

--- test/unit/services/salesServices.js
+++ test/unit/services/salesServices.js
@@ -66,3 +66,43 @@ describe('Busca todos as vendas no Banco de Dados', () => {
   
   });
   });
+  
+  describe('Busca um Sale por ID', () => {
+    describe('Quando uma sale é encontrada', () => {
+      const salesId = [
+        {
+          date: "2022-06-02T19:21:14.000Z",
+          productId: 2,
+          quantity: 10,
+        }
+      ];
+      before( async () => {
+        sinon.stub(saleModel, 'getById').resolves(salesId);
+      });
+      after( () => { saleModel.getById.restore(); })
+      it('Espera que seja um array', async () => {
+        const response = await saleService.getById();
+        expect(response).to.be.an('array');
+      });
+      it('Espera que seja um array que contem um objecto com as chaves "date", "quantity", "productId" ', async () => {
+        const response = await saleService.getById();
+        expect(response[0]).to.be.an('object');
+        expect(response[0]).to.include.all.keys('date', 'quantity', 'productId')
+      });
+    });
+    describe('Quando uma sale  não é encontrada', () => {
+      before( async () => {
+        sinon.stub(saleModel, 'getById').resolves([]);
+      });
+      after( () => { saleModel.getById.restore(); })
+      it('Espera que seja um array', async () => {
+        const response = await saleService.getById();
+        expect(response).to.be.an('array');
+      });
+      it('Espera que seja um array vazio', async () => {
+        const response = await saleService.getById();
+        expect(response).to.be.empty;
+        
+      });
+    });
+  });
